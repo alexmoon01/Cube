d8fb39b788382bbc25a4a711c5fee1cae4d105f4
Alexander Moon <ajmoon2@wisc.edu>
2018-12-17 19:08:50 -0600

    Built constructor for Cube object
    Built a (bad) constructor for a six-sided cube with variable side colors. It's weird, don't look at it too hard

--- src/cubeSim/Cube.java
+++ src/cubeSim/Cube.java
@@ -1,53 +1,63 @@
 package cubeSim;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Point;
 import javax.swing.JComponent;
 
 /**
  * The cube class. This will contain all of the information and the draw method
- * for the cube.
+ * for a cube centered on zero.
  * @author Alex
  */
-public class Cube extends JComponent {
-  private final int SIDE_LENGTH; //The length in pixels of each side of the cube
-  private final Color[] FACE_COLOR; //An array of the colors of the faces of the cube
+public class Cube extends JComponent implements Viewable {
+  private final Polygon[] polygons;
   
   /**
-   * Location of the viewer in spherical coordinates with the origin
-   * at the center of the cube. {radius, polar angle, azimuthal angle}
+   * Instantiates the attributes of the cube centered on zero
+   * @param sideLength The length of a side of the cube
    */
-  private double[] viewerLoc;
-  
-  /**
-   * Instantiates the attributes of the cube
-   * @param side The side length in pixels of the cube
-   */
-  public Cube(int side) {
-    SIDE_LENGTH = side;
-    
-    FACE_COLOR = new Color[6];
-    //Assigns six distinct colors to the sides of the cube.
-    //[1] = top face, [2] = bottom face,
-    //[3] = left face, [4] = front face,
-    //[5] = right face, [6] = back face
-    for (int i = 0; i < FACE_COLOR.length; i++) {
-      FACE_COLOR[i] = new Color(42 * i, 255 - 42 * i, (int)(Math.random() * 255));
+  public Cube(int sideLength) {
+    polygons = new Polygon[6];
+    for (int i = 0; i < polygons.length; i++) {
+      Color color = new Color(252 - 42 * i, 42 * i, (int)(Math.random() * 256));
+      int constantPlane; //The corner that is held constant while the others vary to make a square
+      int varPlane1; //variable corners
+      int varPlane2; //variable corners
+      int alternator = (i % 2) * 2 - 1; //Alternates the fixed point's sign
+      Point3d[] points = new Point3d[4]; //The set of points that will be passed into the sides
+      //Assigning the correct corners to hold still
+      if (i <= 2) {
+        constantPlane = 0;
+        varPlane1 = 1;
+        varPlane2 = 2;
+      } else if (i <= 4) {
+        constantPlane = 1;
+        varPlane1 = 0;
+        varPlane2 = 2;
+      } else {
+        constantPlane = 2;
+        varPlane1 = 0;
+        varPlane2 = 1;
+      }
+      //Going through the corners in order around the perimeter
+      for (int j = -1; j <= 1; j += 2) {
+        for (int k = j; k <= 1 && k >= -1; k += -2 * j) {
+          int[] coordinates = new int[3];
+          coordinates[constantPlane] = alternator * sideLength;
+          coordinates[varPlane1] = j * sideLength;
+          coordinates[varPlane2] = k * sideLength;
+          points[((j + 1) / 2) + (Math.abs(k - j) / 2)] =
+              new Point3d(coordinates[0], coordinates[1], coordinates[2]);
+        }
+      }
+      //Adding the square to the set of polygons
+      polygons[i] = new Polygon(points, color);
+      
     }
-    
-    viewerLoc = new double[3];
+  }
+
+  @Override
+  public Polygon[] getPolygons() {
+    return polygons;
   }
   
-  /**
-   * Draws the cube on the graphics environment.
-   * @param g The canvas on which we will draw.
-   */
-  public void draw(Graphics g) {
-    //This array holds the point locations of the four corners of
-    //every face. The face number is the first value, faces are listed
-    //in the same order as in the face color array. Corners are
-    //top left, bottom left, bottom right, top right.
-    Point[][] faceCorners = new Point[6][4];
-  }
 }
